bff64b8647df99fb1eaa88b9e7ffc063f7b88f11
kimbadanti <shaw08kimbadanti@gmail.com>
2024-06-22 08:26:31 +0530

    correcting setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,26 @@ from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
-def get_requirments(file_path:str)->List[str]:
-    requirments=[]
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
     with open(file_path) as file_obj:
-        requirments=file_obj.readlines()
-        requirments=[req.replace("\n","")for req in requirments]
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPEN_E_DOT in requirments:
-            requirments.remove(HYPEN_E_DOT)
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
 setup(
-    name='mlproject',
-    version='0.0.1',
-    author='Kimbadanti shaw',
-    author_email='shaw08kimbadanti@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirments('requirments.txt')
+name='mlproject',
+version='0.0.1',
+author='Kim',
+author_email='shaw08kimbadanti@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
 )
