a3437f92af0b18e5766c07ff4208eba28b2d299c
kimbadanti <shaw08kimbadanti@gmail.com>
2024-06-22 09:32:45 +0530

    completing traning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -25,7 +25,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('stud.csv')
+            df=pd.read_csv('dataset\stud.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -39,7 +39,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            logging.info("Ingestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -48,7 +48,8 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e,sys)
-        
+    
+    
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
@@ -58,5 +59,4 @@ if __name__=="__main__":
 
     modeltrainer=ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
-
 
